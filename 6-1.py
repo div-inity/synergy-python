a0397f0739d81c38108c66b9ef71c4c36fcffc6b
anna <veselaya.devka@ya.ru>
2026-02-09 15:55:09 +0300

    Лишнее удалено

--- 6-1.py
+++
@@ -1,7 +0,0 @@
-n = int(input())
-m = []
-for i in range(n) :
-    m.append(int(input()))
-
-m.reverse()
-print(*m)
--- 8-2.py
+++
@@ -1,5 +0,0 @@
-my_dict = dict()
-for i in range(10, -6, -1):
-    my_dict[i] = i ** i
-
-print(my_dict)
